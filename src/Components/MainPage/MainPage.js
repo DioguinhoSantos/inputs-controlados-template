4f2b92eb10f9966b48708331fba06cd3221bcc3b
DioguinhoSantos <diiogosantos@live.com>
2022-12-08 14:47:47 -0300

    farmacia0812

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,17 +2,35 @@ import React from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+
+  const onChangeNome = (event) => {
+    console.log(event.target.value)
+  }
+
+  const onChangeIdade = (event) => {
+    console.log(event.target.value)
+  }
+
+  const onChangeEmail = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input onChange={onChangeNome}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input onChange={onChangeIdade}/>
+        </label>
+        <label>
+          E-mail:
+          <Input onChange={onChangeEmail}/>
+        </label>
       <button>Enviar dados</button>
       </Form>
     </MainContainer>
